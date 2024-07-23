bf898cecc782b70c0f47e8f080421fb32e53d8b9
John Doe <gamingskeleton3@gmail.com>
2024-07-23 07:37:06 -0700

    mid redo of the UI

---
+++ src/main/java/com/zonespace/rpplayerinfo/client/gui/BigTextEditBox.java
@@ -0,0 +1,22 @@
+package com.zonespace.rpplayerinfo.client.gui;
+
+import javax.annotation.Nullable;
+
+import com.mojang.blaze3d.vertex.PoseStack;
+
+import net.minecraft.client.gui.Font;
+import net.minecraft.client.gui.components.EditBox;
+import net.minecraft.network.chat.Component;
+
+public class BigTextEditBox extends EditBox {
+
+    public BigTextEditBox(Font p_94114_, int p_94115_, int p_94116_, int p_94117_, int p_94118_, Component p_94119_) {
+        this(p_94114_, p_94115_, p_94116_, p_94117_, p_94118_, (EditBox)null, p_94119_);
+   }
+
+    public BigTextEditBox(Font p_94106_, int p_94107_, int p_94108_, int p_94109_, int p_94110_, @Nullable EditBox p_94111_, Component p_94112_) {
+        super(p_94106_, p_94107_, p_94108_, p_94109_, p_94110_, p_94111_, p_94112_);
+    }
+
+    // Ideally this should allow for a player to input a large amt of info into a chatbox and it should newline when one fills and etc. but it's beyond my knowledge atm
+}
--- src/main/java/com/zonespace/rpplayerinfo/client/gui/RPInfoScreen.java
+++ src/main/java/com/zonespace/rpplayerinfo/client/gui/RPInfoScreen.java
@@ -1,14 +1,23 @@
 package com.zonespace.rpplayerinfo.client.gui;
 
 import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.FormattedText;
+import net.minecraft.network.chat.Style;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.annotation.Nonnull;
 
+import com.mojang.blaze3d.platform.Lighting;
 import com.mojang.blaze3d.systems.RenderSystem;
 import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.math.Quaternion;
+import com.mojang.math.Vector3f;
 import com.zonespace.rpplayerinfo.RPPlayerInfo;
 import com.zonespace.rpplayerinfo.client.ClientPlayerRPData;
 import com.zonespace.rpplayerinfo.data.EPlayerPermission;
@@ -19,13 +28,17 @@ import com.zonespace.rpplayerinfo.networking.ModMessages;
 import com.zonespace.rpplayerinfo.networking.packet.PlayerDataSyncC2SPacket;
 
 import net.minecraft.ChatFormatting;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.components.Button;
 import net.minecraft.client.gui.components.EditBox;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.client.renderer.GameRenderer;
+import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.renderer.entity.EntityRenderDispatcher;
 
 public class RPInfoScreen extends Screen {
     private static final Component TITLE = Component.translatable("gui." + RPPlayerInfo.MODID + ".rp_info_screen");
+    private static final Component DESC_TITLE = Component.translatable("gui." + RPPlayerInfo.MODID + ".rp_info_screen.desc_title");
 
     private static final Component PERMISSION_BUTTON_NO = Component.translatable("gui." + RPPlayerInfo.MODID + ".rp_info_screen.button.permission_button.no").withStyle(ChatFormatting.BOLD);
     private static final Component PERMISSION_BUTTON_ASK = Component.translatable("gui." + RPPlayerInfo.MODID + ".rp_info_screen.button.permission_button.ask").withStyle(ChatFormatting.BOLD);
@@ -43,6 +56,13 @@ public class RPInfoScreen extends Screen {
     private HeightEditBox feetEditBox;
     private HeightEditBox inchesEditBox;
     private EditBox nameEditBox;
+    private EditBox descriptionEditBox;
+
+    private Button permissionToKillButton;
+    private Button permissionToMaimButton;
+    private Button descriptionCloseButton;
+    private Button descriptionViewEditButton;
+
 
     //@SuppressWarnings("unused")
     //private Button button;
@@ -55,9 +75,9 @@ public class RPInfoScreen extends Screen {
         super(TITLE);
 
         this.targetPlayer = targetPlayer;
-        this.imageWidth = 176;
-        this.imageHeight = 166;
-        this.isOwner = false;
+        this.imageWidth = 220;
+        this.imageHeight = 224;
+        this.isOwner = true;
     }
 
     public void onClose() {
@@ -75,6 +95,9 @@ public class RPInfoScreen extends Screen {
         }
         if(nameEditBox != null) {
             ClientPlayerRPData.setName(nameEditBox.getValue());
+        }
+        if(descriptionEditBox != null) {
+            ClientPlayerRPData.setDescription(descriptionEditBox.getValue());
         }
         super.onClose();
     }
@@ -116,23 +139,40 @@ public class RPInfoScreen extends Screen {
                 this::onPermissionToMaimButtonPress
             ));
         });*/
+        descriptionCloseButton = addRenderableWidget(new Button(
+            leftPos + 136,
+            topPos + 140,
+            34,
+            20,
+            Component.translatable("gui.rpplayerinfo.rp_info_screen.button.close"),
+            this::onDescCloseButtonPress
+        ));
+        descriptionCloseButton.visible = false;
+        /*descriptionEditBox = addRenderableWidget(new EditBox(
+            font, 
+            this.leftPos + 18, 
+            this.topPos + 20, 
+            140, 
+            80, 
+            Component.literal(String.valueOf(ClientPlayerRPData.getName()))));
+        descriptionEditBox.setVisible(false);*/
         if(this.isOwner) {
-            Component permissionToKillButton = convertPermissionEnumToButton(ClientPlayerRPData.getPermissionToKill());
-            addRenderableWidget(new Button(
+            Component permissionToKillButtonComp = convertPermissionEnumToButton(ClientPlayerRPData.getPermissionToKill());
+            permissionToKillButton = addRenderableWidget(new Button(
                 leftPos + 106,
                 topPos + 135,
                 34,
                 20,
-                permissionToKillButton,
+                permissionToKillButtonComp,
                 this::onPermissionToKillButtonPress
             ));
-            Component permissionToMaimButton = convertPermissionEnumToButton(ClientPlayerRPData.getPermissionToMaim());
-            addRenderableWidget(new Button(
+            Component permissionToMaimButtonComp = convertPermissionEnumToButton(ClientPlayerRPData.getPermissionToMaim());
+            permissionToMaimButton = addRenderableWidget(new Button(
                 leftPos + 106,
                 topPos + 110,
                 34,
                 20,
-                permissionToMaimButton,
+                permissionToMaimButtonComp,
                 this::onPermissionToMaimButtonPress
             ));
             feetEditBox = addRenderableWidget(new HeightEditBox(
@@ -140,7 +180,7 @@ public class RPInfoScreen extends Screen {
                 this.leftPos + 45, 
                 this.topPos + 45, 
                 16, 
-                20, 
+                16, 
                 Component.literal(String.valueOf(ClientPlayerRPData.getHeightFeet()))));
             feetEditBox.setMaxLength(1);
             feetEditBox.setSuggestion("5", true);
@@ -150,7 +190,7 @@ public class RPInfoScreen extends Screen {
                 this.leftPos + 67, 
                 this.topPos + 45, 
                 24, 
-                20, 
+                16, 
                 Component.literal(String.valueOf(ClientPlayerRPData.getHeightInches()))));
             inchesEditBox.setMaxLength(2);
             inchesEditBox.setSuggestion("9", true);
@@ -162,7 +202,33 @@ public class RPInfoScreen extends Screen {
                 120, 
                 20, 
                 Component.literal(String.valueOf(ClientPlayerRPData.getName()))));
-            nameEditBox.setValue(String.valueOf(ClientPlayerRPData.getName()));            
+            nameEditBox.setValue(String.valueOf(ClientPlayerRPData.getName()));   
+            /*descriptionViewEditButton = addRenderableWidget(new Button(
+                leftPos + 68,
+                topPos + 64,
+                34,
+                20,
+                Component.translatable("gui.rpplayerinfo.rp_info_screen.button.edit"),
+                this::onDescOpenButtonPress
+            ));*/
+            descriptionEditBox = addRenderableWidget(new EditBox(
+                font, 
+                leftPos + 68, 
+                topPos + 64, 
+                100, 
+                20, 
+                Component.literal(ClientPlayerRPData.getDescription())));
+            descriptionEditBox.setValue(ClientPlayerRPData.getDescription());
+            descriptionEditBox.setMaxLength(440); // this should be scaled up if the rp menu is
+        } else {
+            descriptionViewEditButton = addRenderableWidget(new Button(
+                leftPos + 68,
+                topPos + 64,
+                34,
+                20,
+                Component.translatable("gui.rpplayerinfo.rp_info_screen.button.view"),
+                this::onDescOpenButtonPress
+            ));   
         }
         
     }
@@ -202,6 +268,38 @@ public class RPInfoScreen extends Screen {
         });
     }
 
+    private void onDescOpenButtonPress(Button button) {
+        isViewingDescription = true;
+        if(isOwner) {
+            permissionToKillButton.visible = false;
+            permissionToMaimButton.visible = false;
+            feetEditBox.setVisible(false);
+            inchesEditBox.setVisible(false);
+            nameEditBox.setVisible(false);
+            descriptionEditBox.setVisible(true);
+        }
+        descriptionCloseButton.visible = true;
+        if(descriptionViewEditButton != null) {
+            descriptionViewEditButton.visible = false;
+        }
+    }
+
+    private void onDescCloseButtonPress(Button button) {
+        isViewingDescription = false;
+        if(isOwner) {
+            permissionToKillButton.visible = true;
+            permissionToMaimButton.visible = true;
+            feetEditBox.setVisible(true);
+            inchesEditBox.setVisible(true);
+            nameEditBox.setVisible(true);
+            descriptionEditBox.setVisible(false);
+        }
+        descriptionCloseButton.visible = false;
+        if(descriptionViewEditButton != null) {
+            descriptionViewEditButton.visible = true;
+        }
+    }
+
     @Override
     public void render(@Nonnull PoseStack graphics, int mouseX, int mouseY, float partialTicks) {
         renderBackground(graphics);
@@ -212,32 +310,46 @@ public class RPInfoScreen extends Screen {
         blit(graphics, leftPos, topPos, 0, 0, imageWidth, imageHeight);
         super.render(graphics, mouseX, mouseY, partialTicks);
 
-        font.draw(graphics, TITLE, this.leftPos + 8, this.topPos + 8, 0x404040);
+        if(!isViewingDescription) {
+            font.draw(graphics, TITLE, this.leftPos + 8, this.topPos + 8, 0x2e2d2d);
 
-        font.draw(graphics, Component.translatable("gui." + RPPlayerInfo.MODID + ".rp_info_screen.string.name").getVisualOrderText(), this.leftPos + 5, this.topPos + 25, 0x404040);
-        if(!this.isOwner) {
-            font.draw(graphics, Component.literal(ClientPlayerRPData.getName()).withStyle(ChatFormatting.BOLD), this.leftPos + 40, this.topPos + 25, 0x404040);
-        }
+            font.draw(graphics, Component.translatable("gui." + RPPlayerInfo.MODID + ".rp_info_screen.string.name").getVisualOrderText(), this.leftPos + 5, this.topPos + 25, 0x404040);
+            if(!this.isOwner) {
+                font.draw(graphics, Component.literal(ClientPlayerRPData.getName()).withStyle(ChatFormatting.BOLD), this.leftPos + 40, this.topPos + 25, 0x404040);
+            }
 
-        font.draw(graphics, Component.translatable("gui." + RPPlayerInfo.MODID + ".rp_info_screen.string.height").getVisualOrderText(), this.leftPos + 5, this.topPos + 50, 0x404040);
-        if(!this.isOwner) {
-            font.draw(graphics, Component.literal(String.valueOf(ClientPlayerRPData.getHeightFeet()) + "'" + String.valueOf(ClientPlayerRPData.getHeightInches()) + "\"").withStyle(ChatFormatting.BOLD), this.leftPos + 45, this.topPos + 50, 0x404040);
+            font.draw(graphics, Component.translatable("gui." + RPPlayerInfo.MODID + ".rp_info_screen.string.height").getVisualOrderText(), this.leftPos + 5, this.topPos + 50, 0x404040);
+            if(!this.isOwner) {
+                font.draw(graphics, Component.literal(String.valueOf(ClientPlayerRPData.getHeightFeet()) + "'" + String.valueOf(ClientPlayerRPData.getHeightInches()) + "\"").withStyle(ChatFormatting.BOLD), this.leftPos + 45, this.topPos + 50, 0x404040);
+            } else {
+                font.draw(graphics, "'", this.leftPos + 63, this.topPos + 46, 0x404040);
+                font.draw(graphics, "\"", this.leftPos + 93, this.topPos + 46, 0x404040);
+            }
+            
+            font.draw(graphics, Component.translatable("gui." + RPPlayerInfo.MODID + ".rp_info_screen.string.desc").getVisualOrderText(), this.leftPos + 5, this.topPos + 70, 0x404040);
+            
+            //font.draw(graphics, Component.literal(ClientPlayerRPData.getDescription()).withStyle(ChatFormatting.BOLD), this.leftPos + 45, this.topPos + 30, 0x404040);
+
+            font.draw(graphics, Component.translatable("gui." + RPPlayerInfo.MODID + ".rp_info_screen.string.ptk").getVisualOrderText(), this.leftPos + 5, this.topPos + 140, 0xFF0000);
+            font.draw(graphics, Component.translatable("gui." + RPPlayerInfo.MODID + ".rp_info_screen.string.ptm").getVisualOrderText(), this.leftPos + 5, this.topPos + 115, 0xFF0000);
+            if(!this.isOwner) {
+                font.draw(graphics, convertPermissionEnumToButton(ClientPlayerRPData.getPermissionToKill()), this.leftPos + 106, this.topPos + 140, 0xFF0000);
+                font.draw(graphics, convertPermissionEnumToButton(ClientPlayerRPData.getPermissionToMaim()), this.leftPos + 106, this.topPos + 115, 0xFF0000);
+            }
+            renderPlayer(leftPos + 180, topPos + 78, 32, (float)(leftPos + 180) - mouseX, (float)(topPos + 78 - 50) - mouseY, targetPlayer);
         } else {
-            font.draw(graphics, "'", this.leftPos + 63, this.topPos + 46, 0x404040);
-            font.draw(graphics, "\"", this.leftPos + 93, this.topPos + 46, 0x404040);
-        }
-        
-        font.draw(graphics, Component.translatable("gui." + RPPlayerInfo.MODID + ".rp_info_screen.string.desc").getVisualOrderText(), this.leftPos + 5, this.topPos + 70, 0x404040);
-        // implement desc viewing later
-        
-        //font.draw(graphics, Component.literal(ClientPlayerRPData.getDescription()).withStyle(ChatFormatting.BOLD), this.leftPos + 45, this.topPos + 30, 0x404040);
+            font.draw(graphics, Component.literal(ClientPlayerRPData.getName() + Component.translatable("gui.rpplayerinfo.rp_info_screen.string.desc.title").getString()), this.leftPos + 8, this.topPos + 8, 0x2e2d2d);
+            if(!isOwner) {
+                int n = 0;
+                for(FormattedText text : splitDescriptionString()) {
+                    font.draw(graphics, Component.literal(text.getString()), this.leftPos + 6, this.topPos + 24 + n, 0x404040);
+                    n += 8;
+                }
 
-        font.draw(graphics, Component.translatable("gui." + RPPlayerInfo.MODID + ".rp_info_screen.string.ptk").getVisualOrderText(), this.leftPos + 5, this.topPos + 140, 0xFF0000);
-        font.draw(graphics, Component.translatable("gui." + RPPlayerInfo.MODID + ".rp_info_screen.string.ptm").getVisualOrderText(), this.leftPos + 5, this.topPos + 115, 0xFF0000);
-        if(!this.isOwner) {
-            font.draw(graphics, convertPermissionEnumToButton(ClientPlayerRPData.getPermissionToKill()), this.leftPos + 106, this.topPos + 140, 0xFF0000);
-            font.draw(graphics, convertPermissionEnumToButton(ClientPlayerRPData.getPermissionToMaim()), this.leftPos + 106, this.topPos + 115, 0xFF0000);
+                //font.draw(graphics, Component.literal(ClientPlayerRPData.getDescription()), this.leftPos + 6, this.topPos + 30, 0x404040);
+            }
         }
+
     }
 
     public static Component convertPermissionEnumToButton(EPlayerPermission permissionEnum) {
@@ -261,6 +373,62 @@ public class RPInfoScreen extends Screen {
             return EPlayerPermission.PERMISSION_NO;
         }
     }
+
+    private List<FormattedText> splitDescriptionString() {
+        return font.getSplitter().splitLines(ClientPlayerRPData.getDescription(), 163, Style.EMPTY);
+    }
+
+    public static void renderPlayer(int p_98851_, int p_98852_, int p_98853_, float p_98854_, float p_98855_, LivingEntity p_98856_) {
+        float f = (float)Math.atan((double)(p_98854_ / 40.0F));
+        float f1 = (float)Math.atan((double)(p_98855_ / 40.0F));
+        renderPlayerRaw(p_98851_, p_98852_, p_98853_, f, f1, p_98856_);
+    }
+
+    @SuppressWarnings("deprecation")
+    public static void renderPlayerRaw(int p_98851_, int p_98852_, int p_98853_, float angleXComponent, float angleYComponent, LivingEntity p_98856_) {
+        PoseStack posestack = RenderSystem.getModelViewStack();
+        posestack.pushPose();
+        posestack.translate((double)p_98851_, (double)p_98852_, 1050.0);
+        posestack.scale(1.0F, 1.0F, -1.0F);
+        RenderSystem.applyModelViewMatrix();
+        PoseStack posestack1 = new PoseStack();
+        posestack1.translate(0.0, 0.0, 1000.0);
+        posestack1.scale((float)p_98853_, (float)p_98853_, (float)p_98853_);
+        Quaternion quaternion = Vector3f.ZP.rotationDegrees(180.0F);
+        Quaternion quaternion1 = Vector3f.XP.rotationDegrees(angleYComponent * 20.0F);
+        quaternion.mul(quaternion1);
+        posestack1.mulPose(quaternion);
+        float f2 = p_98856_.yBodyRot;
+        float f3 = p_98856_.getYRot();
+        float f4 = p_98856_.getXRot();
+        float f5 = p_98856_.yHeadRotO;
+        float f6 = p_98856_.yHeadRot;
+        p_98856_.yBodyRot = 180.0F + angleXComponent * 20.0F;
+        p_98856_.setYRot(180.0F + angleXComponent * 40.0F);
+        p_98856_.setXRot(-angleYComponent * 20.0F);
+        p_98856_.yHeadRot = p_98856_.getYRot();
+        p_98856_.yHeadRotO = p_98856_.getYRot();
+        Lighting.setupForEntityInInventory();
+        EntityRenderDispatcher entityrenderdispatcher = Minecraft.getInstance().getEntityRenderDispatcher();
+        quaternion1.conj();
+        entityrenderdispatcher.overrideCameraOrientation(quaternion1);
+        entityrenderdispatcher.setRenderShadow(false);
+        MultiBufferSource.BufferSource multibuffersource$buffersource = Minecraft.getInstance().renderBuffers().bufferSource();
+        RenderSystem.runAsFancy(() -> {
+            entityrenderdispatcher.render(p_98856_, 0.0, 0.0, 0.0, 0.0F, 1.0F, posestack1, multibuffersource$buffersource, 15728880);
+        });
+        multibuffersource$buffersource.endBatch();
+        entityrenderdispatcher.setRenderShadow(true);
+        p_98856_.yBodyRot = f2;
+        p_98856_.setYRot(f3);
+        p_98856_.setXRot(f4);
+        p_98856_.yHeadRotO = f5;
+        p_98856_.yHeadRot = f6;
+        posestack.popPose();
+        RenderSystem.applyModelViewMatrix();
+        Lighting.setupFor3DItems();
+    }
+
 
    /* @Override
     public boolean isPauseScreen() {
