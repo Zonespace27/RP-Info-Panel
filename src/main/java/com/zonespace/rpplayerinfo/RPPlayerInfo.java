8f9c547427753f0ab9c95f3e165fc27d0fb4993b
John Doe <gamingskeleton3@gmail.com>
2024-11-15 17:49:00 -0800

    Russian localization + fix for dying resetting your player info for others

--- src/main/java/com/zonespace/rpplayerinfo/RPPlayerInfo.java
+++ src/main/java/com/zonespace/rpplayerinfo/RPPlayerInfo.java
@@ -8,9 +8,11 @@ import com.zonespace.rpplayerinfo.networking.packet.PlayerDataSyncS2CPacket;
 import com.zonespace.rpplayerinfo.networking.packet.PlayerLoginSyncS2CPacket;
 
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.GameType;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.common.capabilities.RegisterCapabilitiesEvent;
@@ -69,8 +71,15 @@ public class RPPlayerInfo
         @SubscribeEvent
         public static void onAttachCapabilitiesPlayer(AttachCapabilitiesEvent<Entity> event) {
             if(event.getObject() instanceof Player) {
-                if(!event.getObject().getCapability(PlayerRPDataProvider.PLAYER_RP_DATA).isPresent()) {
+                Player ourPlayer = (Player)event.getObject();
+                if(!ourPlayer.getCapability(PlayerRPDataProvider.PLAYER_RP_DATA).isPresent()) {
                     event.addCapability(new ResourceLocation(MODID, "properties"), new PlayerRPDataProvider());
+                    /*if(ourPlayer instanceof ServerPlayer) {
+                        ServerPlayer ourServerPlayer = (ServerPlayer)ourPlayer;
+                        if(ourServerPlayer.connection != null) {
+                            ModMessages.sendToPlayer(new PlayerLoginSyncS2CPacket(), ourServerPlayer);
+                        }
+                    }*/
                 }
             }
         }
@@ -78,12 +87,30 @@ public class RPPlayerInfo
         @SubscribeEvent
         public static void onPlayerCloned(PlayerEvent.Clone event) {
             if(event.isWasDeath()) {
-                event.getOriginal().getCapability(PlayerRPDataProvider.PLAYER_RP_DATA).ifPresent(oldStore -> {
-                    event.getOriginal().getCapability(PlayerRPDataProvider.PLAYER_RP_DATA).ifPresent(newStore -> {
+                event.getOriginal().reviveCaps();
+                event.getOriginal().getCapability(PlayerRPDataProvider.PLAYER_RP_DATA).resolve().ifPresent(oldStore -> {
+                    event.getEntity().getCapability(PlayerRPDataProvider.PLAYER_RP_DATA).resolve().ifPresent(newStore -> {
                         newStore.copyFrom(oldStore);
+                        MinecraftServer theServer = event.getEntity().getLevel().getServer();
+                        if(theServer != null) { 
+                            ModMessages.sendToPlayer(new PlayerLoginSyncS2CPacket(), (ServerPlayer)event.getEntity());
+                            ModMessages.sendToPlayer(new PlayerLoginSyncS2CPacket(), (ServerPlayer)event.getOriginal());
+                        }
                     });
                 });
             }
+        }
+
+        @SubscribeEvent
+        public static void onPlayerChangeGameMode(PlayerEvent.PlayerChangeGameModeEvent event) {
+            /*if(event.getCurrentGameMode() == GameType.SPECTATOR) { 
+                event.getEntity().getCapability(PlayerRPDataProvider.PLAYER_RP_DATA).resolve().ifPresent(newStore -> {
+                    MinecraftServer theServer = event.getEntity().getLevel().getServer();
+                    if(theServer != null) { 
+                        ModMessages.sendToPlayer(new PlayerLoginSyncS2CPacket(), (ServerPlayer)event.getEntity());
+                    }
+                });
+            }*/
         }
         
         @SubscribeEvent
@@ -101,5 +128,4 @@ public class RPPlayerInfo
             ModMessages.sendToPlayer(new PlayerLoginSyncS2CPacket(), serverPlayer);
         }
     }
-
 }
